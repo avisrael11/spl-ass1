436536c923e0cd8ec4fcbbecd020d529edb201bc
oradrok <oradrok@users.noreply.github.com>
2017-11-23 03:30:46 +0200

    rm command

--- src/RmCommand.cpp
+++ src/RmCommand.cpp
@@ -2,27 +2,38 @@
 // Created by Aviv Israel on 15/11/2017.
 //
 #include "../include/Commands.h"
+#include "../include/NevigationHelper.h"
+#include "../include/Files.h"
+
+#include <iostream>
 
 using namespace std;
 
-/*class RmCommand : public BaseCommand {
-private:
-public:
-    RmCommand(string args);
-    void execute(FileSystem & fs);
-    string toString();
-};
-*/
 
 RmCommand::RmCommand(string args) : BaseCommand(args) {
 
 }
 
 void RmCommand::execute(FileSystem &fs) {
+	string path = getArgs();
 
+	NevigationHelper nh;
+	BaseFile* toRemove = nh.getBaseFileFromPath(fs, path);
+
+	if (toRemove == nullptr) {
+		cout << "No such file or directory" << endl;
+		return;
+	}
+	if (toRemove->isFile()) {
+		nh.getDeepestDirectoryInPath(fs, path).removeFile(toRemove);
+	}
+	else {
+		((Directory*)toRemove)->getParent()->removeFile(toRemove);
+	}
+	
 }
 
 string RmCommand::toString() {
-    return std::string();
+    return "rm";
 }
 
